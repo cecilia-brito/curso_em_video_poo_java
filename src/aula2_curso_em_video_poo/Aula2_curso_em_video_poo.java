5be8619e41477707b42aa117ba49504d83cd4899
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2022-04-11 09:17:08 -0300

    criando pasta

---
+++ src/aula2_curso_em_video_poo/Aula2_curso_em_video_poo.java
@@ -0,0 +1,19 @@
+
+package aula2_curso_em_video_poo;
+
+/**
+ *
+ * @author cecil
+ */
+public class Aula2_curso_em_video_poo {
+
+
+    public static void main(String[] args) {
+        pencil p1 = new pencil();
+        p1.cor = "preto";
+        p1.ponta = 0.7f;
+        p1.estaTampada = true;
+        p1.status();
+    }
+    
+}
---
+++ src/aula2_curso_em_video_poo/pencil.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula2_curso_em_video_poo;
+
+/**
+ *
+ * @author cecil
+ */
+public class pencil {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean estaTampada;
+    
+    void status(){
+        System.out.println("Uma caneta " + this.cor);
+    }
+    
+    void rabiscar(){
+        
+    }
+    
+    void tampar(){
+        
+    }
+    
+    void destampar(){
+        
+    }
+}
